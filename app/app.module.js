a6dd60d10613356995274c66f2bcdc3ed9572012
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-02-06 13:36:14 -0600

    converted to IIFEs

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,5 @@
-var app = angular.module("monitor-the-web", ['ngRoute', 'ngMaterial']);
+(function() {
+  'use strict';
+  var app = angular.module("app", ['ngRoute', 'ngMaterial']);
+
+})();
--- app/app.routes.js
+++ app/app.routes.js
@@ -1,9 +1,19 @@
-app.config(['$routeProvider',
-  function($routeProvider){
-    $routeProvider
+(function () {
+
+  'use strict';
+
+  angular
+      .module("app")
+      .config(config);
+
+    function config ($routeProvider) {
+      $routeProvider
       // .when('/', {
       //   templateUrl: 'app/components/auth/authView.html',
       //   controller: 'AuthCtrl as authCtrl'
       // })
       .otherwise({redirectTo: '/'});
-  }]);
+    };
+
+
+})();
